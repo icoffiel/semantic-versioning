5b7905ce3bd585b8ebd3519e88f911215336936f
Iain Coffield <icoffiel@Iains-MacBook-Pro.local>
2017-11-03 13:01:35 -0500

    Initial Commit

---
+++ src/main/java/com/example/semanticversioning/SemanticVersioningApplication.java
@@ -0,0 +1,12 @@
+package com.example.semanticversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticVersioningApplication.class, args);
+	}
+}
